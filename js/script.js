9b1d9b4ec1b769195b60cb0bf06e69850d3bf9f6
Mirthe Lenssen <50704463+mirthelenssen@users.noreply.github.com>
2020-09-10 10:26:52 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $( function() {
   $( function() {
     $( "#tabs" ).tabs();
   } );
+  $( "#sound" ).click(function() {
+    $( "#toggle" ).toggle( "slide" );
+     $("#themesound").css("display", "block");
+    if ($(this).text() == "Stop Playing Theme Song") { 
+      $(this).text("Play Breaking Bad Theme Song"); 
+      $(this).css("color", "black");
+
+  } else { 
+      $(this).text("Stop Playing Theme Song"); 
+      $(this).css("color", "black");
+
+  }; 
+  });
+ 
